7569325aaf4a9846692dace44f77dea12816d066
smudge37 <stephenm.jackson@btinternet.com>
2017-11-13 15:02:35 +0000

    Next installment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const readline = require('readline-sync');
+
 console.log("Welcome to the calculator!");
+
+//Input
+console.log("Enter a number");
+const no1 = Number(readline.prompt());
+console.log("Enter another number");
+const no2 = Number(readline.prompt());
+
+//Multiply and out
+out = no1*no2;
+console.log(no1 + "x" + no2 + "=" + out);
