09b5454bb8a35430525a7c929a21920df04dba87
smudge37 <stephenm.jackson@btinternet.com>
2017-11-20 16:50:39 +0000

    Stuff with for loops

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const readline = require('readline-sync');
+var i=0;
 
 console.log("Welcome to the calculator!");
 console.log("==========================");
@@ -10,7 +11,7 @@ let numTries = 1;
 while ( op!="/" && op!="*" && op!="-" && op!="+"){
 	switch (numTries){
 		case 1:
-			console.log("That was not a valid operator. /n Please enter a valid one.")
+			console.log("That was not a valid operator. \nPlease enter a valid one.")
 			break;
 		case 2:
 			console.log("Nope, that one neither. This next one better be valid or you'll be outta here.")
@@ -21,28 +22,44 @@ while ( op!="/" && op!="*" && op!="-" && op!="+"){
 	op = readline.prompt();
 	numTries++;
 }
-console.log("Enter a number.");
-const no1 = Number(readline.prompt());
-console.log("Enter another number.");
-const no2 = Number(readline.prompt());
+
+console.log("How many numbers do you want to " + op + "?");
+const numCount = readline.prompt();
+var nums = Array(numCount);
+for (i=0; i<numCount; i++){
+	console.log("Enter a number.");
+	nums[i] = +readline.prompt();
+}
 
 //Operate and out
 switch(op){
 	case "/":
-		out = no1/no2;
+		out = nums[0];
+		for (i=1; i < numCount; i++){
+			out = out/nums[i];
+		}
 		break;
 	
 	case "*":
-		out = no1*no2;
+		out = 1;
+		for (i=0; i < numCount; i++){
+			out = out*num[i];
+		}
 		break;
 	
 	case "-":
-		out = no1-no2;
+		out = nums[0];
+		for (i=1; i < numCount; i++){
+			out -= nums[i];
+		}
 		break;
 	
 	default:
-		out = no1+no2;
+		out = 0;
+		for (i=0; i < numCount; i++){
+			out += nums[i];
+		}
 		break;
 	
 }
-console.log(no1 + op + no2 + "=" + out);
+console.log(nums.join( " "+op+" ") + "=" + out);
